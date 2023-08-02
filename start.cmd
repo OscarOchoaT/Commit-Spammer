8e8a00437e10af74f2fa410171a7c8c9e1003aab
NulledCodeDev <oscarfdo2004@gmail.com>
2023-08-02 14:48:18 -0500

    Commit 4003

--- start.cmd
+++ start.cmd
@@ -3,7 +3,7 @@
 rem Crea el archivo file.txt con contenido vacío
 echo. > file.txt
 
-for /L %%i in (3907,1,4000) do (
+for /L %%i in (4003,1,5000) do (
     rem Añade una línea al archivo file.txt
     echo Commit número %%i >> file.txt
 
